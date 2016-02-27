2f3a8615591b8541fe0ec9149eab9a87dc1b18d4
luckydonald <luckydonald@flutterb.at>
2016-02-27 18:34:55 +0100

    Added Docstrings

--- src/main/java/de/luckydonald/utils/Functions.java
+++ src/main/java/de/luckydonald/utils/Functions.java
@@ -9,9 +9,34 @@ import de.luckydonald.utils.ObjectWithLogger;
  * @since 18.02.2016
  **/
 public class Functions extends ObjectWithLogger {
+    /**
+     * Forwards the call to the {@link #getMethodName(int)} method to get the caller name without specifying {@code offset = 0}<br>
+     * {@code
+     *   void foo() {
+     *      getMethodName() // == "foo"
+     *      }
+     * }
+     *
+     * @implNote This calls {@code getMethodName(1)}, because {@code 0} would be <b>this</b> method.
+     *           If you call {@link #getMethodName(int) it} directly, use {@code 0} as parameter.
+     * @return the name.
+     */
     public static String getMethodName () {
-        return getMethodName(0);
+        return getMethodName(1); // because 0 is this function.
+        // if you're calling getMethodName(int offset) you can use 0 instead.
     }
+
+    /**
+     * get the name of the method. {@code 0} is the calling method (<i>"you"</i>),
+     * {@code 1} is the method which called <i>you</i>.<br>
+     * {@code
+     *   void foo() {
+     *      getMethodName(0) // == "foo"
+ *      }
+     * }
+     * @param offset
+     * @return
+     */
     public static String getMethodName (int offset) {
         StackTraceElement ste[] = Thread.currentThread().getStackTrace();
         String thisClass = "";
